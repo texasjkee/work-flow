3022bfb731284c3c08bdfcdd4300f47cea0bf5c1
TexasJkee <gt.gluck@gmail.com>
2025-01-23 09:50:27 +0200

    add: silent

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -27,7 +27,7 @@ return {
 			vim.keymap.set("n", "gh", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set("n", "gD", ":Telescope lsp_definitions<CR>", { silent = true })
-			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { silent = true })
 			vim.keymap.set("n", "<leader>.", vim.lsp.buf.code_action, {})
 
 			vim.keymap.set("n", "<leader>dj", vim.diagnostic.goto_next, {})
