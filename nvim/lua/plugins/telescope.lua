cc050e0426b80640af00dadcdf03619a4ddf3ea0
TexasJkee <gt.gluck@gmail.com>
2025-01-24 10:19:09 +0200

    add: telescope icnons

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -4,6 +4,7 @@ return {
 		tag = "0.1.8",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
 			{
 				"nvim-telescope/telescope-fzf-native.nvim",
 				build = "make",
