75c5ea0c4efb084399e00da7c9efd9887cc3c037
TexasJkee <gt.gluck@gmail.com>
2025-01-31 12:41:58 +0200

    delete pinerose theme

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -15,14 +15,3 @@ return {
 		vim.cmd([[colorscheme tokyonight]])
 	end,
 }
-
--- return {
--- 	"rose-pine/neovim",
--- 	name = "rose-pine",
--- 	config = function()
--- 		require("rose-pine").setup({
--- 			disable_background = true, -- Off background
--- 		})
--- 		vim.cmd("colorscheme rose-pine")
--- 	end,
--- }
