e8c44d2ddd104fa5e005aba52b48d0d7686adcc9
TexasJkee <gt.gluck@gmail.com>
2025-01-31 11:56:02 +0200

    add: tokyonight

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,10 +1,28 @@
 return {
-  "rose-pine/neovim",
-  name = "rose-pine",
-  config = function()
-    require("rose-pine").setup({
-      disable_background = true, -- Off background
-    })
-    vim.cmd("colorscheme rose-pine")
-  end,
+	"folke/tokyonight.nvim",
+	lazy = false,
+	priority = 1000,
+	-- Off background
+	opts = {
+		transparent = true,
+		styles = {
+			sidebars = "transparent",
+			floats = "transparent",
+		},
+	},
+	config = function(_, opts)
+		require("tokyonight").setup(opts)
+		vim.cmd([[colorscheme tokyonight]])
+	end,
 }
+
+-- return {
+-- 	"rose-pine/neovim",
+-- 	name = "rose-pine",
+-- 	config = function()
+-- 		require("rose-pine").setup({
+-- 			disable_background = true, -- Off background
+-- 		})
+-- 		vim.cmd("colorscheme rose-pine")
+-- 	end,
+-- }
