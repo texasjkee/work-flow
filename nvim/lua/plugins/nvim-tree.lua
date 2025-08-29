a0a2b6ba698be3b915d6096e60e465bfced20cd3
texasjkee <gt.gluck@gmail.com>
2025-08-29 10:19:59 +0000

    added: show .env files

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,35 +1,41 @@
 return {
-	"nvim-tree/nvim-tree.lua",
-	lazy = false, -- Load immediately
-	config = function()
-		require("nvim-tree").setup({
-			-- Current file in tree
-			respect_buf_cwd = true,
-			update_focused_file = {
-				enable = true,
-				update_root = true,
-			},
-			view = {
-				width = 999,
-				side = "left",
-				preserve_window_proportions = false,
-			},
-			actions = {
-				open_file = {
-					quit_on_open = true,
-					window_picker = { enable = false },
-				},
-			},
-			renderer = {
-				icons = {
-					show = {
-						file = false,
-						folder = false,
-						folder_arrow = false,
-						git = false,
-					},
-				},
-			},
-		})
-	end,
+  "nvim-tree/nvim-tree.lua",
+  lazy = false, -- Load immediately
+  config = function()
+    require("nvim-tree").setup({
+      respect_buf_cwd = true,
+      update_focused_file = {
+        enable = true,
+        update_root = true,
+      },
+      view = {
+        width = 999,
+        side = "left",
+        preserve_window_proportions = false,
+      },
+      actions = {
+        open_file = {
+          quit_on_open = true,
+          window_picker = { enable = false },
+        },
+      },
+      renderer = {
+        icons = {
+          show = {
+            file = false,
+            folder = false,
+            folder_arrow = false,
+            git = false,
+          },
+        },
+      },
+      git = {
+        enable = true,
+        ignore = false,
+      },
+      filters = {
+        dotfiles = false,
+      },
+    })
+  end,
 }
