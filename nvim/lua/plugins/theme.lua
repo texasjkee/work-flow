dcc39e055dc3b2513d359f13d1c920cb2f452c56
TexasJkee <gt.gluck@gmail.com>
2025-09-20 20:50:58 +0300

    added: rose pine theme

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,20 +1,31 @@
+-- return {
+-- 	"folke/tokyonight.nvim",
+-- 	lazy = false,
+-- 	priority = 1000,
+-- 	-- Off background
+-- 	opts = {
+-- 		transparent = true,
+-- 		styles = {
+-- 			sidebars = "transparent",
+-- 			floats = "transparent",
+-- 		},
+-- 	},
+-- 	config = function(_, opts)
+-- 		require("tokyonight").setup(opts)
+-- 		vim.cmd([[colorscheme tokyonight]])
+-- 		vim.cmd([[
+-- 			highlight NvimTreeNormal guifg=#ffffff
+-- 		]])
+-- 	end,
+-- }
+
 return {
-	"folke/tokyonight.nvim",
-	lazy = false,
-	priority = 1000,
-	-- Off background
-	opts = {
-		transparent = true,
-		styles = {
-			sidebars = "transparent",
-			floats = "transparent",
-		},
-	},
-	config = function(_, opts)
-		require("tokyonight").setup(opts)
-		vim.cmd([[colorscheme tokyonight]])
-		vim.cmd([[
-			highlight NvimTreeNormal guifg=#ffffff
-		]])
+	"rose-pine/neovim",
+	name = "rose-pine",
+	config = function()
+		require("rose-pine").setup({
+			disable_background = true, -- Off background
+		})
+		vim.cmd("colorscheme rose-pine")
 	end,
 }
